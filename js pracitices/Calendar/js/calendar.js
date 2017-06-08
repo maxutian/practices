171dba018dea11676810a9632258e7d7c40d8d1c
maxutian <maxutian95@gmail.com>
2017-06-08 22:26:19 +0800

    'prenext'

--- js pracitices/Calendar/js/calendar.js
+++ js pracitices/Calendar/js/calendar.js
@@ -1,23 +1,39 @@
-var mon = document.getElementById('Monday');
-var tue = document.getElementById('Tuesday');
-var wed = document.getElementById('Wednesday');
-var thur = document.getElementById('Thursday');
-var fri = document.getElementById('Friday');
-var sat = document.getElementById('Sataurday');
-var sun = document.getElementById('Sunday');
-var year = document.getElementById('year');
-var month = document.getElementById('month');
+function $ (node) {
+	return document.getElementById(node);
+}
+
+var year = new Date().getFullYear();
+var month = new Date().getMonth();
+var yearNode = $('year');
+var monthNode = $('month');
+var mon = $('Monday');
+var tue = $('Tuesday');
+var wed = $('Wednesday');
+var thur = $('Thursday');
+var fri = $('Friday');
+var sat = $('Sataurday');
+var sun = $('Sunday');
 var weekdays = [mon,tue,wed,thur,fri,sat,sun];
 
 function initDate () {
-	year.innerText = new Date().getFullYear();
-	month.innerText = new Date().getMonth() + 1;
+	yearNode.innerText = year;
+	monthNode.innerText = month + 1;
+	setDate(year, month);
+	changeDate();
+};
+
+function clearDate () {
+	for (var i in weekdays){
+		while (weekdays[i].children[1]) {
+			weekdays[i].removeChild(weekdays[i].children[1]);
+		}
+	}
 }
 
 function setDate (year, month) {
-	var sumDate = new Date(year, month, 0).getDate();
+	var sumDate = new Date(year, month + 1, 0).getDate();
 	// 留白
-	var firstDate = new Date(year, month - 1, 0).getDay();
+	var firstDate = new Date(year, month, 0).getDay();
 	for (var i = 0; i < firstDate; i++) {
 		var datep = document.createElement('p');
 		datep.className = 'date';
@@ -25,7 +41,7 @@ function setDate (year, month) {
 	}
 	// 添加日期
 	for (var i = 0;i < sumDate;i++){
-		var weekday = new Date(year, month - 1, i).getDay();
+		var weekday = new Date(year, month, i).getDay();
 		var datep = document.createElement('p');
 		datep.className = 'date';
 		var datenum = document.createTextNode(i + 1);
@@ -56,7 +72,43 @@ function setDate (year, month) {
 	}
 }
 
-window.onload = function () {
-	initDate();
-	setDate(year.innerText, month.innerText);
+function changeDate () {
+	var preY = $('preYear');
+	var preM = $('preMonth');
+	var nextY = $('nextYear');
+	var nextM = $('nextMonth');
+
+	preY.addEventListener('click', function () {
+		clearDate();
+		setDate(--year, month);
+		yearNode.innerText = parseInt(yearNode.innerText) - 1;
+	});
+
+	preM.addEventListener('click', function () {
+		clearDate();
+		setDate(year, --month);
+		if (parseInt(monthNode.innerText) === 1) {
+			monthNode.innerText = 12;
+			yearNode.innerText = parseInt(yearNode.innerText) - 1;
+		} else {
+		  monthNode.innerText = parseInt(monthNode.innerText) - 1;
+		}
+	});
+
+	nextY.addEventListener('click', function () {
+		clearDate();
+		setDate(++year, month);
+		yearNode.innerText = parseInt(yearNode.innerText) + 1;
+	});
+
+	nextM.addEventListener('click', function () {
+		clearDate();
+		setDate(year, ++month);
+		if (parseInt(monthNode.innerText) === 12) {
+			monthNode.innerText = 1;
+			yearNode.innerText = parseInt(yearNode.innerText) + 1;
+		} else {
+		  monthNode.innerText = parseInt(monthNode.innerText) + 1;
+		}
+	});
 }
