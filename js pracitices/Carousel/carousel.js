98624e10653f2366ba78a3ba6f29831252c06ebc
maxutian <maxutian95@gmail.com>
2017-07-14 17:06:58 +0800

    back_up

--- js pracitices/Carousel/carousel.js
+++ js pracitices/Carousel/carousel.js
@@ -1,24 +1,56 @@
-var imgArr = ['style_001.jpg', 'style_002.jpg', 'style_003.jpg', 'style_004.jpg'];
+var Settings = {
+	imgWidth: '20%',
+	imgRatio: '1',
+	imgArr: ['style_001.jpg', 'style_002.jpg', 'style_003.jpg', 'style_004.jpg']
+}
 
-function Carousel (imgArr, width) {
+function Carousel () {
 
-	this.imgWidth = width;
+	var holder = document.getElementById('Carousel'),
+			showWindow = document.getElementById('show');
 
 	this.initResource = (function () {
 
-		var holder = document.querySelector('#Carousel');
-
-		for (var i in imgArr) {
+		// 依次获取图片资源并布局到页面中
+		for (var i in Settings.imgArr) {
 
 			var imgHolder = document.createElement('img');
-			imgHolder.setAttribute('class', 'img' + ++i);
-			imgHolder.src = 'images/' + imgArr[--i];
-			imgHolder.width = this.imgWidth * holder.width;
+
+			imgHolder.setAttribute('class', 'img');
+			imgHolder.setAttribute('id', 'img' + ++i)
+			imgHolder.src = 'images/' + Settings.imgArr[--i];
 			holder.appendChild(imgHolder);
 
 		}
-		
+
+		// 调整图片比例
+		imgRatio();
+
 	})();
+
+	this.formatImages = (function () {
+
+		// 监听窗口大小并调整比例
+		window.onresize = function () {
+
+			imgRatio();
+
+		}
+
+	})();
+
+	function imgRatio () {
+
+		for (var i in Settings.imgArr) {
+
+				var images = document.getElementById('img' + ++i);
+
+				showWindow.style.width = images.style.width = Settings.imgWidth;
+				showWindow.style.height = images.style.height = (images.clientWidth / Settings.imgRatio) + 'px';
+
+			}
+
+	}
 
 	this.autoScroll = function () {
 
@@ -40,4 +72,4 @@ function Carousel (imgArr, width) {
 
 }
 
-var initCarousel = new Carousel(imgArr);
+var initCarousel = new Carousel();
